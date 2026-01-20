db8d5db9ba1ff3124c9464dac4bb3c64bc6b76e7
joonwon2003 <joonwon2003@gmail.com>
2026-01-20 13:01:12 +0900

    2026.01.20.1300
    아이템들 구현
    -광원 효과와 파티클 추가

---
+++ Assets/1Scripts/Item.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.InputSystem.Controls;
+
+public class Item : MonoBehaviour
+{
+    public enum Type { Ammo,Coin,Grenade,Heart,Weapon };
+    public Type type;
+    public int value;
+
+     void Update()
+    {
+        transform.Rotate(Vector3.up * 20 * Time.deltaTime);
+    }
+}
